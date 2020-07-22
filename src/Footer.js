056af004f74bc28cf2eead05ef130a74c5ce782e
BradMoore <48004534+developingdeveloper0@users.noreply.github.com>
2020-07-22 11:33:23 -0400

    initial layout

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = (props) => {
+	return (
+		<div className="footer">
+			<p>Footer Information will go here</p>
+
+		</div>
+
+	)
+}
+
+export default Footer;
