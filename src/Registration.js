23786eeeee987fe85f0de682200ef9f7bee875f0
BradMoore <48004534+developingdeveloper0@users.noreply.github.com>
2020-07-23 20:25:39 -0400

    updates to login form

--- src/Registration.js
+++ src/Registration.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import Footer from './Footer';
 import './Registration.css';
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 import { Button, Form, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 
 
 const Registration = (props) => {
 	return (
-		<div>
+		<div className="all">
 			<div className="container">
 				<Login />
-			<Footer />
+				<Footer />
 		</div>
 		</div>
 
@@ -19,6 +20,7 @@ const Registration = (props) => {
 function Login(props) {
 	return (
 		<div className="login">
+		<Router>
 		<Form>
 		  <Form.Group controlId="formBasicEmail">
 		    <Form.Label>Email address</Form.Label>
@@ -32,11 +34,30 @@ function Login(props) {
 		    <Form.Label>Password</Form.Label>
 		    <Form.Control type="password" placeholder="Password" />
 		  </Form.Group>
+		  <div className="loginButton">
 		  <Button variant="primary" className="btn-dark" type="submit">
 		    Login
 		  </Button>
+		  </div>
+		  <br/>
+		  <div className = "noAccount">
+		  <p>No account yet?<Link to="/register"><p className="registerbtn">Register Now</p></Link></p>  
+		  </div>
 		</Form>
+
+		</Router>
+		<Switch>
+			<Route path='/register'>
+				<Register />
+			</Route>
+		</Switch>
 		</div>
+		);
+}
+
+function Register(props) {
+	return (
+		<h1>Change component working</h1>
 		);
 }
 
