80cd23a31f2c8b8008343fa92c0911b99c48cfb3
BradMoore <48004534+developingdeveloper0@users.noreply.github.com>
2020-09-01 15:21:53 -0400

    Added search bar to stock finder page

--- src/Finder.js
+++ src/Finder.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import Footer from './Footer';
+import Button from 'react-bootstrap/Button';
+import FormControl from 'react-bootstrap/FormControl';
+import InputGroup from 'react-bootstrap/InputGroup';
+import './Finder.css';
+var unirest = require('unirest');
 
 
 const Finder = (props) => {
 	return (
-		<div>
-			<div className="jumbotron">
-				<h1>Page for finding and adding stocks to your list</h1>
-			</div>
+		<div className='container'>
+			<InputGroup className="mb-3">
+			    <FormControl
+			      placeholder="Stock Symbol"
+			      aria-label="Recipient's username"
+			      aria-describedby="basic-addon2"
+			    />
+			    <InputGroup.Append>
+			      <Button variant="outline-secondary">Search</Button>
+			    </InputGroup.Append>
+			  </InputGroup>
 			<Footer />
 		</div>
 
