f38e87a0fce47eb0ba647f236bba43c4da40886c
BradMoore <48004534+developingdeveloper0@users.noreply.github.com>
2020-09-07 13:20:01 -0400

    Added function that grabs searchvalue, need to add state to update API call with new symbol

--- src/Finder.js
+++ src/Finder.js
@@ -4,38 +4,48 @@ import Button from 'react-bootstrap/Button';
 import FormControl from 'react-bootstrap/FormControl';
 import InputGroup from 'react-bootstrap/InputGroup';
 import './Finder.css';
-var unirest = require('unirest');
-var req = unirest("GET", "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-summary");
 
-req.query({
-	"region": "US",
-	"symbol": "TSLA"
-});
-
-req.headers({
-	"x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
-	"x-rapidapi-key": "c2e612e4b4mshdc0b42705d50512p115877jsn9f363acad0b8",
-	"useQueryString": true
-});
-
-
-req.end(function (res) {
-	if (res.error) throw new Error(res.error);
-
-	console.log(res.body.price.regularMarketPrice["raw"]);
-});
 
 const Finder = (props) => {
+	var sym = "TSLA";
+
+	var unirest = require('unirest');
+	var req = unirest("GET", "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-summary");
+
+	req.query({
+		"region": "US",
+		"symbol": sym
+	});
+
+	req.headers({
+		"x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
+		"x-rapidapi-key": "c2e612e4b4mshdc0b42705d50512p115877jsn9f363acad0b8",
+		"useQueryString": true
+	});
+
+
+	req.end(function (res) {
+		if (res.error) throw new Error(res.error);
+
+		console.log(res.body.price.regularMarketPrice["raw"]);
+	});
+
+	const searchSymbol = () => {
+		sym = document.getElementById("search").value;
+		console.log(sym);
+	}
+
 	return (
 		<div className='container'>
 			<InputGroup className="mb-3">
 			    <FormControl
+			      id = "search"
 			      placeholder="Stock Symbol"
 			      aria-label="Recipient's username"
 			      aria-describedby="basic-addon2"
 			    />
 			    <InputGroup.Append>
-			      <Button variant="outline-secondary">Search</Button>
+			      <Button  onClick = {searchSymbol} variant="outline-secondary">Search</Button>
 			    </InputGroup.Append>
 			  </InputGroup>
 			<Footer />
